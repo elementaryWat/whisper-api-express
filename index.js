6528b3a3bb4abb45deded8a73f4bd7b9f203f01b
Augusto Romero <augustoromerodev@gmail.com>
2023-04-17 19:28:36 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,57 @@
+require("dotenv").config();
+const express = require("express");
+const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
+const cors = require("cors");
+const { Configuration, OpenAIApi } = require("openai");
+
+const app = express();
+app.use(cors());
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    const ext = path.extname(file.originalname);
+    const basename = path.basename(file.originalname, ext);
+    cb(null, `${basename}-${Date.now()}${ext}`);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+app.use(express.json());
+
+app.post("/transcribe", upload.single("audio"), async (req, res) => {
+  try {
+    const audioPath = req.file.path;
+    console.log(audioPath);
+    const resp = await openai.createTranscription(
+      fs.createReadStream(audioPath),
+      "whisper-1"
+    );
+
+    fs.unlink(audioPath, (err) => {
+      if (err) {
+        console.error("Error deleting the audio file:", err);
+      }
+    });
+
+    res.json(resp.data);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "An error occurred while transcribing the audio." });
+  }
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
